c4922401ee56b9c8a8a1f30b05e0dba7031e8002
nikesh chaudhari <info.nikesh12@gmail.com>
2025-09-16 17:19:02 +0545

    Get USers

--- route/users.js
+++ route/users.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const route = express.Router();
 const db = require("../db.config");
-
+const bcypt = require("bcrypt");
 route.post("/add-user", async (req, res) => {
   try {
+    const hash = bcypt.hash(req.body.password, 10);
     const name = req.body.name;
     const email = req.body.email;
-    const password = req.body.password;
+    const password = hash;
 
     const query = "INSERT INTO users(name,email,password)VALUES(?,?,?)";
 
@@ -28,6 +29,32 @@ route.post("/add-user", async (req, res) => {
     console.log("Not insert");
     res.status(500).json({
       error: err,
+    });
+  }
+});
+
+// get users
+route.get("/users", async (req, res) => {
+  try {
+    const query = "SELECT * FROM users";
+    db.query(query, (err, result) => {
+      if (err) {
+        console.log("something Wrong..", err);
+        return res.status(500).json({
+          error: "USer Not found..",
+        });
+      } else {
+        console.log("All Users");
+      }
+      res.status(200).json({
+        msg:"All users",
+        id:result.ViesId
+      })
+    });
+  } catch (err) {
+    console.log("Error found");
+    res.status(500).json({
+      error:err
     });
   }
 });
